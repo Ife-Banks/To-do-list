9d047b93cabab1cfa807ccff368201efc878f06f
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-04-08 22:26:15 +0100

    functionality adjusted

--- index.js
+++ index.js
@@ -4,15 +4,16 @@
 document.getElementById('ad-btn').addEventListener('click',function(){
 const input = document.getElementById('todo-input');
 taskText = input.value.trim();
-
+//create items
 if (input.value !== "" ){
     const list = document.createElement('li');
     list.className = 'list';
     const text = document.createElement('p');
-
+// storing
 text.textContent = taskText;
 
 console.log();
+//return to null
 input.value = " ";
 
 // Add delete fxn
@@ -34,32 +35,22 @@ modbtn.addEventListener('click', () => {
     inp.placeholder='enter';
    if(modbtn.textContent == 'Modify'){
     inp.value= list.firstElementChild.textContent;
-    modbtn.textContent = 'Save';
     list.replaceChild(inp, text);
+    modbtn.inputElement = inp ;
+    modbtn.textContent = 'Save';
    }else if(modbtn.textContent == 'Save'){
-    const newContent = inp.value.trim();
+    const newContent = modbtn.inputElement.value.trim();
     text.textContent = newContent;
+    list.replaceChild(text, modbtn.inputElement);
     modbtn.textContent = 'Modify';
-    list.replaceChild(newContent, inp);
-
-   }
-    // document.getElementById('todo-list').appendChild(inp);
-}
-
-)
-
-
+   }   // document.getElementById('todo-list').appendChild(inp);
+})
 // Appending
 list.appendChild(text);
 list.append(dltbtn);
 list.append(modbtn);
 document.getElementById('todo-list').appendChild(list);
-
-
-
 }
-   }
-
-)
+   })
 
 
