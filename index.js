3c7019aa91f34ba07ca45254001af9410071eff0
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-04-10 06:20:23 +0100

    Task des

--- index.js
+++ index.js
@@ -1,54 +1,68 @@
 
-// document.getElementById('ad-btn').addEventListener('click',function(){
-// const input = document.getElementById('todo-input');
-// taskText = input.value.trim();
-// //create items
+document.getElementById('ad-btn').addEventListener('click',function(){
+const input = document.getElementById('todo-input');
+taskText = input.value.trim();
+const inputDEs = document.getElementById('todo-des');
+des = inputDEs.value.trim();
+
+// var text;
+//create items
 // if (input.value !== "" ){
 //     const list = document.createElement('li');
 //     list.className = 'list';
-//     const text = document.createElement('p');
+//     text = document.createElement('p');
 // // storing
-// text.textContent = taskText;
+//     text.textContent = taskText;
+// console.log();}
+// return to null
+console.log(taskText);
+saveList(taskText,des);
+console.log(localStorage.getItem('listValue'));
+input.value = " ";
+window.location.href='/public/Dashboard.html'})
 
-// console.log();
-// //return to null
-// input.value = " ";
+// // // Add delete fxn
+// // const dltbtn= document.createElement('button');
+// // dltbtn.className = 'btn-sty';
+// // dltbtn.textContent = 'Delete';
+// // dltbtn.addEventListener('click', () => {
+// //     list.remove();
+// // })
 
-// // Add delete fxn
-// const dltbtn= document.createElement('button');
-// dltbtn.className = 'btn-sty';
-// dltbtn.textContent = 'Delete';
-// dltbtn.addEventListener('click', () => {
-//     list.remove();
-// })
-
-// //Modify fxn
-// const modbtn = document.createElement('button');
-// modbtn.className='btn-sty';
-// modbtn.textContent = 'Modify';
-// modbtn.addEventListener('click', () => {
-//     const inp = document.createElement('input');
-//     inp.className='inpp';
-//     inp.type='text';
-//     inp.placeholder='enter';
-//    if(modbtn.textContent == 'Modify'){
-//     inp.value= list.firstElementChild.textContent;
-//     list.replaceChild(inp, text);
-//     modbtn.inputElement = inp ;
-//     modbtn.textContent = 'Save';
-//    }else if(modbtn.textContent == 'Save'){
-//     const newContent = modbtn.inputElement.value.trim();
-//     text.textContent = newContent;
-//     list.replaceChild(text, modbtn.inputElement);
-//     modbtn.textContent = 'Modify';
-//    }   // document.getElementById('todo-list').appendChild(inp);
-// })
-// // Appending
-// list.appendChild(text);
-// list.append(dltbtn);
-// list.append(modbtn);
-// document.getElementById('todo-list').appendChild(list);
-// }
+// // //Modify fxn
+// // const modbtn = document.createElement('button');
+// // modbtn.className='btn-sty';
+// // modbtn.textContent = 'Modify';
+// // modbtn.addEventListener('click', () => {
+// //     const inp = document.createElement('input');
+// //     inp.className='inpp';
+// //     inp.type='text';
+// //     inp.placeholder='enter';
+// //    if(modbtn.textContent == 'Modify'){
+// //     inp.value= list.firstElementChild.textContent;
+// //     list.replaceChild(inp, text);
+// //     modbtn.inputElement = inp ;
+// //     modbtn.textContent = 'Save';
+// //    }else if(modbtn.textContent == 'Save'){
+// //     const newContent = modbtn.inputElement.value.trim();
+// //     text.textContent = newContent;
+// //     list.replaceChild(text, modbtn.inputElement);
+// //     modbtn.textContent = 'Modify';
+// //    }   // document.getElementById('todo-list').appendChild(inp);
+// // })
+// // // Appending
+// // list.appendChild(text);
+// // list.append(dltbtn);
+// // list.append(modbtn);
+// // document.getElementById('todo-list').appendChild(list);
+// // }
+// console.log(taskText);
+// saveList(text.textContent);
+// console.log(localStorage.getItem('listValue'));
 //    })
 
+function saveList(value,value2){
+    localStorage.setItem('listValue',value);
+    localStorage.setItem('Des',value2)
 
+}
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -9,3 +9,125 @@ document.getElementById('FirstLetter').textContent=a[0];
 
 document.getElementById('Usernam').textContent=' Hello, ' + b;
 
+document.getElementById('add').addEventListener('click',()=>{
+    window.location.href='/public/addTask.html';
+})
+
+document.addEventListener('DOMContentLoaded',function(){
+    const lisValue = localStorage.getItem('listValue');
+    const des =  localStorage.getItem('Des');
+const lisContainer= document.querySelector('.list');
+if(lisValue){
+    //create list and text
+   
+   const list= document.createElement('li');
+   const div = document.createElement('div');
+   const spn =document.createElement('span');
+   list.appendChild(div);
+   list.appendChild(spn);
+   div.className = 'bound';
+   list.className = 'list';
+   text =document.createElement('h1');
+   text.className='list-tit'
+   text.textContent = lisValue;
+    text2=document.createElement('p');
+    text2.className='list-des';
+    text2.textContent=des;
+
+
+   // Add delete fxn
+    const dltbtn= document.createElement('button');
+    dltbtn.className = 'btn-sty';
+    dltbtn.textContent = 'Delete';
+    dltbtn.addEventListener('click', () => {
+        list.remove();
+    })
+
+
+   // Modify fxn
+    const modbtn = document.createElement('button');
+    modbtn.className='btn-sty';
+    modbtn.textContent = 'Modify';
+    modbtn.addEventListener('click', () => {
+        const inp = document.createElement('input');
+        inp.className='inpp';
+        inp.type='text';
+        inp.placeholder='enter';
+       if(modbtn.textContent == 'Modify'){
+        inp.value= list.firstElementChild.textContent;
+        list.replaceChild(inp, text);
+        modbtn.inputElement = inp ;
+        modbtn.textContent = 'Save';
+       }else if(modbtn.textContent == 'Save'){
+        const newContent = modbtn.inputElement.value.trim();
+        text.textContent = newContent;
+        list.replaceChild(text, modbtn.inputElement);
+        modbtn.textContent = 'Modify';
+       }   // document.getElementById('todo-list').appendChild(inp);
+    })
+    
+     div.appendChild(text);
+     div.appendChild(text2);
+   
+   spn.append(dltbtn);
+   spn.append(modbtn);
+   (div);
+  
+   document.getElementById('todo-list').appendChild(list);
+  
+}
+})
+
+
+// document.getElementById('ad-btn').addEventListener('click',function(){
+//     if (input.value !== "" ){
+//         const list = document.createElement('li');
+//         list.className = 'list';
+//         text = document.createElement('p');
+//     // storing
+//         text.textContent = localStorage.getItem('listValue');
+//     console.log();
+//     //return to null
+//     input.value = " ";
+    
+//     // Add delete fxn
+//     const dltbtn= document.createElement('button');
+//     dltbtn.className = 'btn-sty';
+//     dltbtn.textContent = 'Delete';
+//     dltbtn.addEventListener('click', () => {
+//         list.remove();
+//     })
+    
+//     //Modify fxn
+//     const modbtn = document.createElement('button');
+//     modbtn.className='btn-sty';
+//     modbtn.textContent = 'Modify';
+//     modbtn.addEventListener('click', () => {
+//         const inp = document.createElement('input');
+//         inp.className='inpp';
+//         inp.type='text';
+//         inp.placeholder='enter';
+//        if(modbtn.textContent == 'Modify'){
+//         inp.value= list.firstElementChild.textContent;
+//         list.replaceChild(inp, text);
+//         modbtn.inputElement = inp ;
+//         modbtn.textContent = 'Save';
+//        }else if(modbtn.textContent == 'Save'){
+//         const newContent = modbtn.inputElement.value.trim();
+//         text.textContent = newContent;
+//         list.replaceChild(text, modbtn.inputElement);
+//         modbtn.textContent = 'Modify';
+//        }   // document.getElementById('todo-list').appendChild(inp);
+//     })
+//     // Appending
+//     list.appendChild(text);
+//     list.append(dltbtn);
+//     list.append(modbtn);
+//     document.getElementById('todo-list').appendChild(list);
+//     }
+//     console.log(taskText);
+//     saveList(text.textContent);
+//     console.log(localStorage.getItem('listValue'));
+//        })
+
+// // 
